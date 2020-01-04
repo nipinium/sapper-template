e5a6fec065f9938d852927027032dfe14997d8ce
Nipinium <nipinium@gmail.com>
2020-01-04 14:58:23 +0700

    add dependencies to sass output

--- svelte.config.js
+++ svelte.config.js
@@ -4,32 +4,34 @@ const sass = require('node-sass')
 module.exports = {
     preprocess: {
         style: async ({ content, attributes, filename }) => {
+            if (content.length === 0) return { code: content }
+
             const { type, lang } = attributes
             if (type !== 'text/scss' && lang !== 'scss') return
+
             // const prepend_content = '@import "helpers";\n'
             // content = prepend_content + content
 
+            const options = {
+                data: content,
+                sourceMap: true,
+                includePaths: [
+                    path.dirname(filename),
+                    path.resolve(__dirname, 'node_modules'),
+                    path.resolve(__dirname, 'css'),
+                ],
+                outFile: filename + '.css',
+            }
+
             return new Promise((resolve, reject) => {
-                sass.render(
-                    {
-                        data: content,
-                        sourceMap: 'style.css.map',
-                        omitSourceMapUrl: true,
-                        includePaths: [
-                            path.dirname(filename),
-                            path.join(__dirname, 'node_modules'),
-                            path.resolve(__dirname, 'css'),
-                        ],
-                        outFile: 'x',
-                    },
-                    (err, result) => {
-                        if (err) return reject(err)
-                        resolve({
-                            code: result.css.toString(),
-                            map: result.map.toString(),
-                        })
-                    }
-                )
+                sass.render(options, (err, result) => {
+                    if (err) return reject(err)
+                    resolve({
+                        code: result.css.toString(),
+                        map: result.map.toString(),
+                        dependencies: result.stats.includedFiles,
+                    })
+                })
             })
         },
     },
