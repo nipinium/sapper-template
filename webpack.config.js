f5dd759349590a2ab8d842390d6b909c6104d48b
Nipinium <nipinium@gmail.com>
2020-01-26 22:48:37 +0700

    change tab with from 4 spaces to 2 spaces

--- webpack.config.js
+++ webpack.config.js
@@ -8,9 +8,9 @@ const mode = process.env.NODE_ENV
 const dev = mode === 'development'
 
 const alias = {
-    svelte: path.resolve('node_modules', 'svelte'),
-    $src: path.resolve(__dirname, 'src'),
-    $cpn: path.resolve(__dirname, 'src/components'),
+  svelte: path.resolve('node_modules', 'svelte'),
+  $src: path.resolve(__dirname, 'src'),
+  $cpn: path.resolve(__dirname, 'src/components'),
 }
 
 const extensions = ['.mjs', '.js', '.json', '.svelte', '.html']
@@ -23,111 +23,111 @@ const { preprocess } = require('./svelte.config')
 const autoprefixer = require('autoprefixer')
 const cssnano = require('cssnano')({ preset: 'default' })
 const purgecss = require('@fullhuman/postcss-purgecss')({
-    content: ['./src/**/*.html', './src/**/*.svelte'],
-    keyframes: true,
-    whitelistPatterns: [/svelte-/],
-    defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+  content: ['./src/**/*.html', './src/**/*.svelte'],
+  keyframes: true,
+  whitelistPatterns: [/svelte-/],
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
 })
 
 // exports
 
 module.exports = {
-    client: {
-        entry: config.client.entry(),
-        output: config.client.output(),
-        resolve: { alias, extensions, mainFields },
-        module: {
-            rules: [
-                {
-                    test: /\.(svelte|html)$/,
-                    use: {
-                        loader: 'svelte-loader-hot',
-                        options: {
-                            dev,
-                            preprocess,
-                            hydratable: true,
-                            hotReload: true,
-                            emitCss: false,
-                            hotOptions: {
-                                noPreserveState: false, // Default: false
-                                optimistic: true, // Default: false
-                            },
-                        },
-                    },
-                },
-                {
-                    test: /\.s?css$/,
-                    use: [
-                        {
-                            loader: MiniCssExtractPlugin.loader,
-                            options: {
-                                hmr: dev,
-                                reloadAll: true,
-                            },
-                        },
-                        'css-loader',
-                        !dev && {
-                            loader: 'postcss-loader',
-                            options: {
-                                parsers: 'postcss',
-                                plugins: [autoprefixer, cssnano, purgecss],
-                            },
-                        },
-                        {
-                            loader: 'sass-loader',
-                            options: { sourceMap: true },
-                        },
-                    ].filter(Boolean),
-                },
-            ],
+  client: {
+    entry: config.client.entry(),
+    output: config.client.output(),
+    resolve: { alias, extensions, mainFields },
+    module: {
+      rules: [
+        {
+          test: /\.(svelte|html)$/,
+          use: {
+            loader: 'svelte-loader-hot',
+            options: {
+              dev,
+              preprocess,
+              hydratable: true,
+              hotReload: true,
+              emitCss: false,
+              hotOptions: {
+                noPreserveState: false, // Default: false
+                optimistic: true, // Default: false
+              },
+            },
+          },
         },
-        mode,
-        plugins: [
-            dev && new webpack.HotModuleReplacementPlugin(),
-            new webpack.DefinePlugin({
-                'process.browser': true,
-                'process.env.NODE_ENV': JSON.stringify(mode),
-            }),
-            new MiniCssExtractPlugin({
-                // filename: '[name].css',
-                // chunkFilename: '[name].[id].css',
-                ignoreOrder: false, // Enable to remove warnings about conflicting order
-            }),
-        ].filter(Boolean),
-        devtool: dev && 'inline-source-map',
+        {
+          test: /\.s?css$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                hmr: dev,
+                reloadAll: true,
+              },
+            },
+            'css-loader',
+            !dev && {
+              loader: 'postcss-loader',
+              options: {
+                parsers: 'postcss',
+                plugins: [autoprefixer, cssnano, purgecss],
+              },
+            },
+            {
+              loader: 'sass-loader',
+              options: { sourceMap: true },
+            },
+          ].filter(Boolean),
+        },
+      ],
     },
+    mode,
+    plugins: [
+      dev && new webpack.HotModuleReplacementPlugin(),
+      new webpack.DefinePlugin({
+        'process.browser': true,
+        'process.env.NODE_ENV': JSON.stringify(mode),
+      }),
+      new MiniCssExtractPlugin({
+        // filename: '[name].css',
+        // chunkFilename: '[name].[id].css',
+        ignoreOrder: false, // Enable to remove warnings about conflicting order
+      }),
+    ].filter(Boolean),
+    devtool: dev && 'inline-source-map',
+  },
 
-    server: {
-        entry: config.server.entry(),
-        output: config.server.output(),
-        target: 'node',
-        resolve: { alias, extensions, mainFields },
-        externals: Object.keys(pkg.dependencies).concat('encoding'),
-        module: {
-            rules: [
-                {
-                    test: /\.(svelte|html)$/,
-                    use: {
-                        loader: 'svelte-loader-hot',
-                        options: {
-                            css: false,
-                            generate: 'ssr',
-                            dev,
-                            preprocess,
-                        },
-                    },
-                },
-            ],
+  server: {
+    entry: config.server.entry(),
+    output: config.server.output(),
+    target: 'node',
+    resolve: { alias, extensions, mainFields },
+    externals: Object.keys(pkg.dependencies).concat('encoding'),
+    module: {
+      rules: [
+        {
+          test: /\.(svelte|html)$/,
+          use: {
+            loader: 'svelte-loader-hot',
+            options: {
+              css: false,
+              generate: 'ssr',
+              dev,
+              preprocess,
+            },
+          },
         },
-        mode: process.env.NODE_ENV,
-        performance: {
-            hints: false, // it doesn't matter if server.js is large
-        },
+      ],
     },
+    mode: process.env.NODE_ENV,
+    performance: {
+      hints: false, // it doesn't matter if server.js is large
+    },
+  },
 
-    serviceworker: {
-        entry: config.serviceworker.entry(),
-        output: config.serviceworker.output(),
-        mode: process.env.NODE_ENV,
-    },
+  serviceworker: {
+    entry: config.serviceworker.entry(),
+    output: config.serviceworker.output(),
+    mode: process.env.NODE_ENV,
+  },
 }
