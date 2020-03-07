23ca8d997bb9bdb4b9de47d754090e575279ef0c
Nipinium <nipinium@gmail.com>
2020-03-08 03:33:05 +0700

    add ga

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -2,6 +2,17 @@
   import Nav from '../components/Nav.svelte'
 
   export let segment
+
+  import { stores } from '@sapper/app'
+  const { page } = stores()
+
+  $: {
+    if (typeof gtag === 'function') {
+      window.gtag('config', 'UA-XXX', {
+        page_path: $page.path,
+      })
+    }
+  }
 </script>
 
 <style lang="scss">
@@ -13,6 +24,20 @@
     box-sizing: border-box;
   }
 </style>
+
+<svelte:head>
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-XXX">
+
+  </script>
+  <script>
+    window.dataLayer = window.dataLayer || []
+    function gtag() {
+      dataLayer.push(arguments)
+    }
+    gtag('js', new Date())
+    gtag('config', 'UA-XXX')
+  </script>
+</svelte:head>
 
 <Nav {segment} />
 
